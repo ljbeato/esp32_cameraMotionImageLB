b74936c8de314aff32b0620bacd0d8bb679d87d9
Lou Beato <ljbeato@gmail.com>
2022-05-20 22:04:15 -0400

    Matlab code to display image

--- esp32_cameraMotionImageLB.ino
+++ esp32_cameraMotionImageLB.ino
@@ -5,16 +5,17 @@
 
 #include "ESP32Cam.h"
 #include "PlaneLB.h"
+#include "ArduinoComm.h"
 
 
-
-#define THUMB_WIDTH 360
-#define THUMB_HEIGHT 240
+#define CAMERA_WIDTH 360
+#define CAMERA_HEIGHT 240
 int motionCounter = 0;
 
 PlaneLB im;
 PlaneLB prevIm;
 ESP32Cam cam;
+ArduinoComm comm;
 BOOLEAN motionDetected;
 
 void setup() 
@@ -29,15 +30,29 @@ void setup()
     cam.InitCamera(PIXFORMAT_GRAYSCALE);
 }
 
-
+void CheckForTransmit(unsigned char *ptr, int len)
+{
+  //Serial.printf("Length in bytes %d\n", size);
+  if (comm.Available()) 
+  {
+    // read the incoming byte:
+    byte incomingByte = comm.ReadSerialByte();
+    if (incomingByte == 'S')
+    {
+      comm.TransmitRawBytes(ptr, len);
+    }
+  } 
+  
+}
 void loop()
 {
   cam.RedLED(1);
   cam.TakePhoto();
   byte *buf = cam.GetDataBuffer();
   im.set_m_Data(buf);
-  im.DownSample(THUMB_HEIGHT/RESAMPLED_ROWS);
+  im.DownSample(CAMERA_HEIGHT/CAMERA_WIDTH);
   
+  CheckForTransmit(buf, CAMERA_HEIGHT*CAMERA_WIDTH);
   im.DetectMotion(prevIm, motionDetected);
   if (motionDetected == TRUE)
   {
---
+++ plot_ESP32Cam_imageBinary.m
@@ -0,0 +1,103 @@
+% Real time data collection example
+%
+% This script is implemented as a function so that it can
+%   include sub-functions
+%
+% This script can be modified to be used on any platform by changing the
+% serialPort variable.
+% Example:-
+% On Linux:     serialPort = '/dev/ttyS0';
+% On MacOS:     serialPort = '/dev/tty.KeySerial1';
+% On Windows:   serialPort = "\\\\.\\COM5";
+%
+%To run: 
+%plot_cams()
+%To reset ports: (if MATLAB still thinks they're busy)
+%delete(instrfindall)
+%
+function plot_ESP32Cam_imageBinary
+pkg load instrument-control
+supportedinterfaces = instrhwinfo().SupportedInterfaces
+
+if ! isempty(strfind (supportedinterfaces , "serial"))
+    disp("Serial: Supported")
+else
+    disp("Serial: Unsupported")
+endif
+# Opens serial port ttyUSB1 with baudrate of 115200 (config defaults to 8-N-1)
+s1 = serial("\\\\.\\COM34", 115200) 
+# Flush input and output buffers
+srl_flush(s1); 
+set(s1, "baudrate", 115200) # Change baudrate
+set(s1, "bytesize", 8)    # Change byte size (config becomes 5-N-1)
+set(s1, "parity", "N")    # Changes parity checking (config becomes 5-E-1),
+                          # possible values [E]ven, [O]dd, [N]one.
+set(s1, "stopbits", 1)    # Changes stop bits (config becomes 5-E-2), possible
+                          # values 1, 2.
+
+set(s1, "dataterminalready", "off")  # Enables DTR line
+set(s1, "requesttosend", "off")      # Enables RTS line
+                                    # possible values "on", "off". 
+%Send over bluetooth or serial
+%serialPort = '\\\\.\\COM5';
+%serialObject = serial(serialPort);
+%configure serial connection
+%serialObject.BaudRate = 115200; %(Default)
+%serialObject.BaudRate = 115200;
+%serialObject.FlowControl = 'software';
+
+%Initiate serial connection
+%fopen(serialObject);
+
+% This gets called on cleanup to ensure the stream gets closed
+finishup = onCleanup(@() myCleanupFun(s1));
+
+% Instantiate variables
+count = 1;
+row = 1;
+column=1;
+imHeight = 480
+imWidth = 640
+im = zeros (imHeight, imWidth);
+state = 0;
+
+  while (1)
+    % send an S to get started
+    fprintf("START...\n");
+    srl_write(s1, 'S');
+    # Blocking read call, returns uint8 array of exactly 1 bytes read
+    %
+    for row=1:imHeight
+      fprintf ("Reading line=%d of %d\n",row, imHeight);
+      for column=1:imWidth
+      %ne
+
+         val = srl_read(s1, 1);
+         %val
+         im(row, column) = val;         
+      % 
+      endfor        
+    endfor 
+    fprintf ("DONE\n");
+    f = imshow(im, [0,255]);
+    drawnow;        
+
+  end % while(1)
+
+% Clean up the serial object
+fclose(s1);
+
+clear s1;
+
+end %plot_cams
+
+%*****************************************************************************************************************
+%*****************************************************************************************************************
+
+function myCleanupFun(serialObject)
+% Clean up the serial object
+fclose(s1);
+
+clear s1;
+delete(instrfindall);
+end
