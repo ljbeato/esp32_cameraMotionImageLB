92a61fad3b9d2136dc7d24ad9050ea6717e8cf13
Lou Beato <ljbeato@gmail.com>
2022-05-20 23:50:47 -0400

    fixed height width mismatch

--- esp32_cameraMotionImageLB.ino
+++ esp32_cameraMotionImageLB.ino
@@ -7,9 +7,9 @@
 #include "PlaneLB.h"
 #include "ArduinoComm.h"
 
-
-#define CAMERA_WIDTH 360
 #define CAMERA_HEIGHT 240
+#define CAMERA_WIDTH 320
+
 int motionCounter = 0;
 
 PlaneLB im;
@@ -21,13 +21,14 @@ BOOLEAN motionDetected;
 void setup() 
 {
     Serial.begin(115200);
-    im.set_m_iLines(320);
-    im.set_m_iPixels(240);
-    prevIm.set_m_iLines(32);
-    prevIm.set_m_iPixels(24);
+    im.set_m_iLines(CAMERA_HEIGHT);
+    im.set_m_iPixels(CAMERA_WIDTH);
+    prevIm.set_m_iLines(CAMERA_HEIGHT/RESAMPLED_ROWS);
+    prevIm.set_m_iPixels(CAMERA_WIDTH/RESAMPLED_COLS);
     // turn on high freq for fast streaming speed
     Serial.println("Camera init OK");
     cam.InitCamera(PIXFORMAT_GRAYSCALE);
+
 }
 
 void CheckForTransmit(unsigned char *ptr, int len)
@@ -50,8 +51,9 @@ void loop()
   cam.TakePhoto();
   byte *buf = cam.GetDataBuffer();
   im.set_m_Data(buf);
-  im.DownSample(CAMERA_HEIGHT/CAMERA_WIDTH);
-  
+  im.DownSample(CAMERA_HEIGHT/RESAMPLED_ROWS);
+  //for (int i = 0; i < CAMERA_HEIGHT*CAMERA_WIDTH; i++)
+    //buf[i] = (byte)i;
   CheckForTransmit(buf, CAMERA_HEIGHT*CAMERA_WIDTH);
   im.DetectMotion(prevIm, motionDetected);
   if (motionDetected == TRUE)
